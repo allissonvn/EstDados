f0f83c6a325fce369f0e5489f99ca3d7c880e854
Midazsin <allissonviana8@gmail.com>
2025-07-14 23:37:00 -0300

    Decimo Nono Commit

--- src/Ordencao/Selectionsort/SelectionsortDinamicoPrincipal.java
+++ src/Ordencao/Selectionsort/SelectionsortDinamicoPrincipal.java
@@ -1,4 +1,22 @@
 package Ordencao.Selectionsort;
 
+import java.util.Random;
+
 public class SelectionsortDinamicoPrincipal {
+
+    public static void main(String[] args){
+        Random gerador = new Random();
+        int tamanho = 12;
+        SelectionsortDinamico sort = new SelectionsortDinamico();
+
+        for (int i = 0; i < tamanho; i++) {
+            Integer numero = gerador.nextInt(160) + 10;
+            sort.anexar(numero);
+        }
+
+
+        System.out.println(sort.imprimir());
+        sort.ordenar();
+        System.out.println(sort.imprimir());
+    }
 }
