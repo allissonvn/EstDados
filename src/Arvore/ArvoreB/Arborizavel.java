2c4f7c7660aef951fac4ed9659aa0285a1d5c62e
Midazsin <allissonviana8@gmail.com>
2025-07-16 23:27:19 -0300

    Vigesimo Terceiro Commit

---
+++ src/Arvore/ArvoreB/Arborizavel.java
@@ -0,0 +1,27 @@
+package Arvore;
+
+public interface Arborizavel<T extends Comparable<T>> {
+
+    void inserir(T dado);
+
+
+    T apagar(T dado);
+
+
+    boolean existe(T dado);
+
+
+    void limpar();
+
+
+    String imprimirPreOrdem();
+
+
+    String imprimirEmOrdem();
+
+
+    String imprimirPosOrdem();
+
+
+    NoArvoreB<T> getRaiz();
+}
